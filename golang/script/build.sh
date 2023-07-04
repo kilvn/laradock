43aa0e2980a85f3f08c7afe8dcd29e0b0a7ddbe7
逆天西瓜 <content@kilvn.com>
2023-07-04 10:49:30 +0800

    add fdc

--- golang/script/build.sh
+++ golang/script/build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-cd /go/src/ && \
-cd ./financial-master && git pull --no-rebase && go mod tidy && go run ./main.go
+cd /go/src/financial-data-center && git pull --no-rebase && go mod tidy && nohup go run ./main.go 2>&1 &
+
+echo 'financial-data-center started.'
+
+cd /go/src/financial-master && git pull --no-rebase && go mod tidy && go run ./main.go
