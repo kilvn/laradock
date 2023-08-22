18b288d7a47743b2b43087d89196705115e7bdf5
逆天西瓜 <content@kilvn.com>
2023-08-22 10:41:59 +0800

    edit go config

--- golang/script/build.sh
+++ golang/script/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# 修复Windows下的 0500 too open 权限错误问题
+chmod 400 /root/.ssh/id_rsa
+
 cd /go/src/financial-data-center && git pull --no-rebase && go mod tidy && nohup go run ./main.go 2>&1 &
 
 echo 'financial-data-center started.'
