cc7cfaa1b9d441432997d5ef7e6f6a6f64fe235d
逆天西瓜 <content@kilvn.com>
2023-09-28 09:45:31 +0800

    feat: golang改为air

--- golang/script/build.sh
+++ golang/script/build.sh
@@ -3,8 +3,8 @@
 # 修复Windows下的 0500 too open 权限错误问题
 chmod 400 /root/.ssh/id_rsa
 
-cd /go/src/financial-data-center && git pull --no-rebase && go mod tidy && nohup go run ./main.go 2>&1 &
+cd /go/src/financial-data-center && git pull --no-rebase && go mod tidy && nohup air 2>&1 &
 
 echo 'financial-data-center started.'
 
-cd /go/src/financial-master && git pull --no-rebase && go mod tidy && go run ./main.go
+cd /go/src/financial-master && git pull --no-rebase && go mod tidy && air
